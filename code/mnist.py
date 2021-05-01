25a2b4a38c8a84b50de1af4e80aace979abd5ab3
GrantPerkins <gcperk20@gmail.com>
2021-05-01 16:59:20 +0000

    Revert to old mnist, output

--- code/mnist.py
+++ code/mnist.py
@@ -5,29 +5,57 @@ from pathlib import Path
 import tensorflow as tf
 import smdistributed.dataparallel.tensorflow as dist
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
 
-tf.config.experimental.set_visible_devices(gpus[dist.local_rank()], 'GPU')
+def config_gpus():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+    tf.config.experimental.set_visible_devices(gpus[dist.local_rank()], 'GPU')
 
 
-with open("/opt/ml/input/config/hyperparameters.json", 'r') as f:
-    file = json.load(f)
-    epochs = int(file["epochs"])
-    batch_size = int(file["batch_size"])
-    learning_rate = float(file["learning_rate"])
+def get_hyperparameters():
+    with open("/opt/ml/input/config/hyperparameters.json", 'r') as f:
+        file = json.load(f)
+        mnist_epochs = int(file["epochs"])
+        mnist_batch_size = int(file["batch_size"])
+        mnist_learning_rate = float(file["learning_rate"])
+    return mnist_epochs, mnist_batch_size, mnist_learning_rate
 
-data_slice = 'mnist-%d.npz' % dist.rank()
-keras_path = os.path.join(str(Path.home()), ".keras/datasets/")
-os.makedirs(keras_path, exist_ok=True)
-copyfile("/opt/ml/input/data/training/" + data_slice, keras_path + data_slice)
-(mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data(path=data_slice)
-mnist_dataset = tf.data.Dataset.from_tensor_slices(
-    (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32),
-     tf.cast(mnist_labels, tf.int64))
-)
-dataset = mnist_dataset.repeat().shuffle(100000).batch(batch_size)
+
+def get_dataset(mnist_batch_size):
+    data_slice = 'mnist-%d.npz' % dist.rank()
+    keras_path = os.path.join(str(Path.home()), ".keras/datasets/")
+    os.makedirs(keras_path, exist_ok=True)
+    copyfile("/opt/ml/input/data/training/" + data_slice, keras_path + data_slice)
+    (mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data(path=data_slice)
+    mnist_dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32),
+         tf.cast(mnist_labels, tf.int64))
+    )
+    mnist_dataset = mnist_dataset.repeat().shuffle(100000).batch(mnist_batch_size)
+    return mnist_dataset
+
+
+def create_model(mnist_learning_rate):
+    mnist_model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
+        tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    mnist_loss = tf.losses.SparseCategoricalCrossentropy()
+
+    # SMDataParallel: dist.size()
+    # LR for 8 node run : 0.000125
+    # LR for single node run : 0.001
+    mnist_optimizer = tf.optimizers.Adam(mnist_learning_rate * dist.size())
+    tf.train.Checkpoint(model=mnist_model, optimizer=optimizer)
+    return mnist_model, mnist_loss, mnist_optimizer
 
 
 @tf.function
@@ -52,32 +80,22 @@ def training_step(images, labels, first_batch):
     return loss_value
 
 
-dist.init()
-print("Worker number:", dist.rank())
-
-    
-model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
-    tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Dropout(0.25),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
-loss = tf.losses.SparseCategoricalCrossentropy()
-optimizer = tf.optimizers.Adam(learning_rate * dist.size())
+def train(mnist_epochs):
+    for batch, (images, labels) in enumerate(dataset.take(mnist_epochs // dist.size())):
+        loss_value = training_step(images, labels, batch == 0)
+        if batch % 50 == 0 and dist.rank() == 0:
+            print('Step #%d\tLoss: %.6f' % (batch, loss_value))
 
 
-# train
-for batch, (images, labels) in enumerate(dataset.take(epochs // dist.size())):
-    batch_loss_value = training_step(images, labels, batch == 0)
-    if batch % 50 == 0 and dist.rank() == 0:
-        print('Step #%d\tLoss: %.6f' % (batch, batch_loss_value))
-
-# SMDataParallel: Save checkpoints only from master node.
-if dist.rank() == 0:
-    checkpoint_dir = "/opt/ml/model"
-    print("Saving to",checkpoint_dir)
-    model.save(os.path.join(checkpoint_dir, '1'))
+if __name__ == "__main__":
+    dist.init()
+    config_gpus()
+    print("Worker number:", dist.rank())
+    epochs, batch_size, learning_rate = get_hyperparameters()
+    model, loss, optimizer = create_model(learning_rate)
+    dataset = get_dataset(batch_size)
+    train(epochs)
+    # SMDataParallel: Save checkpoints only from master node.
+    if dist.rank() == 0:
+        checkpoint_dir = "/opt/ml/model"
+        model.save(os.path.join(checkpoint_dir, '1'))
