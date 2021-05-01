47713dd59b0573fb088695383ce12ba3344a1d35
GrantPerkins <gcperk20@gmail.com>
2021-04-30 22:30:25 -0400

    Idiomatic Python

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# CS4513 Final Project
+# Grant Perkins CS 4513 Final Project
 
-For this project, I created a distributed neural network training solution on AWS for recognizing hand-written digits (MNIST dataset). All code is written by me, Grant Perkins, and some by AWS.
+In this project, I developed a distributed machine learning solution with AWS SageMaker.
+
+Features:
+ - Distributed computing:
+   + One virtual computer runs this Jupyter Notebook
+   + One virtual computer stores dataset
+   + N virtual computers run the training job
+   + One virtual computer runs the inference job (small load)
+ - Machine Learning:
+   + Trains a custom neural network to recognize digits in the MNIST dataset
+   + Customizable hyperparameters (epochs, batch size, learning rate)
+   + Inference on whatever image (classification)
+
+I started this project on 4/30/2021, for CS 4513.
+
+## Section 1: Data
+
+In this section, I download the dataset, and upload it to a computer controlled by AWS S3, a long-term data service.
+
+## Section 2: Training
+
+Now that the data is in the correct place, I can train a neural network for the dataset. I do this on N separate computers. N can be any natural number.
+
--- container/mnist.py
+++ container/mnist.py
@@ -1,89 +1,100 @@
-import tensorflow as tf
-
-# Import SMDataParallel TensorFlow2 Modules
-import smdistributed.dataparallel.tensorflow as dist
-
 import os
 import json
 from shutil import copyfile
+from pathlib import Path
+import tensorflow as tf
+import smdistributed.dataparallel.tensorflow as dist
 
-dist.init()
-print("Dist size",dist.size(), "rank:",dist.rank())
-with open("/opt/ml/input/config/hyperparameters.json", 'r') as f:
-    print("Hyperparameters:",json.load(f))
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-if gpus:
-    # SMDataParallel: Pin GPUs to a single SMDataParallel process [use SMDataParallel local_rank() API]
-    print("there are GPUS")
+
+def config_gpus():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
     tf.config.experimental.set_visible_devices(gpus[dist.local_rank()], 'GPU')
 
-data_slice = 'mnist-%d.npz' % dist.rank()
-from pathlib import Path
-home = str(Path.home())
-keras_path = os.path.join(home,".keras/datasets/")
-os.makedirs(keras_path, exist_ok=True)
-copyfile("/opt/ml/input/data/training/"+data_slice,keras_path+data_slice)
-(mnist_images, mnist_labels), _ = \
-    tf.keras.datasets.mnist.load_data(path=data_slice)
 
-dataset = tf.data.Dataset.from_tensor_slices(
-    (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32),
-             tf.cast(mnist_labels, tf.int64))
-)
-dataset = dataset.repeat().shuffle(100000000).batch(64)
+def get_hyperparameters():
+    with open("/opt/ml/input/config/hyperparameters.json", 'r') as f:
+        file = json.load(f)
+        mnist_epochs = int(file["epochs"])
+        mnist_batch_size = int(file["batch_size"])
+        mnist_learning_rate = float(file["learning_rate"])
+    return mnist_epochs, mnist_batch_size, mnist_learning_rate
 
-mnist_model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
-    tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Dropout(0.25),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
-loss = tf.losses.SparseCategoricalCrossentropy()
 
-# SMDataParallel: dist.size()
-# LR for 8 node run : 0.000125
-# LR for single node run : 0.001
-optimizer = tf.optimizers.Adam(0.000125 * dist.size())
+def get_dataset(mnist_batch_size):
+    data_slice = 'mnist-%d.npz' % dist.rank()
+    keras_path = os.path.join(str(Path.home()), ".keras/datasets/")
+    os.makedirs(keras_path, exist_ok=True)
+    copyfile("/opt/ml/input/data/training/" + data_slice, keras_path + data_slice)
+    (mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data(path=data_slice)
+    mnist_dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32),
+         tf.cast(mnist_labels, tf.int64))
+    )
+    mnist_dataset = mnist_dataset.repeat().shuffle(100000000).batch(mnist_batch_size)
+    return mnist_dataset
 
-checkpoint_dir = "/opt/ml/model"
 
-checkpoint = tf.train.Checkpoint(model=mnist_model, optimizer=optimizer)
+def create_model(mnist_learning_rate):
+    mnist_model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
+        tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    mnist_loss = tf.losses.SparseCategoricalCrossentropy()
+
+    # SMDataParallel: dist.size()
+    # LR for 8 node run : 0.000125
+    # LR for single node run : 0.001
+    mnist_optimizer = tf.optimizers.Adam(mnist_learning_rate * dist.size())
+    mnist_checkpoint = tf.train.Checkpoint(model=mnist_model, optimizer=mnist_optimizer)
+    return mnist_model, mnist_loss, mnist_optimizer, mnist_checkpoint
 
 
 @tf.function
-def training_step(images, labels, first_batch):
+def training_step(images, labels, first_batch, mnist_model, mnist_loss, mnist_optimizer):
     with tf.GradientTape() as tape:
         probs = mnist_model(images, training=True)
-        loss_value = loss(labels, probs)
+        loss_value = mnist_loss(labels, probs)
 
     # SMDataParallel: Wrap tf.GradientTape with SMDataParallel's DistributedGradientTape
     tape = dist.DistributedGradientTape(tape)
 
     grads = tape.gradient(loss_value, mnist_model.trainable_variables)
-    optimizer.apply_gradients(zip(grads, mnist_model.trainable_variables))
+    mnist_optimizer.apply_gradients(zip(grads, mnist_model.trainable_variables))
 
     if first_batch:
-       # SMDataParallel: Broadcast model and optimizer variables
-       dist.broadcast_variables(mnist_model.variables, root_rank=0)
-       dist.broadcast_variables(optimizer.variables(), root_rank=0)
-    
+        # SMDataParallel: Broadcast model and optimizer variables
+        dist.broadcast_variables(mnist_model.variables, root_rank=0)
+        dist.broadcast_variables(mnist_optimizer.variables(), root_rank=0)
+
     # SMDataParallel: all_reduce call
-    loss_value = dist.oob_allreduce(loss_value) # Average the loss across workers
+    loss_value = dist.oob_allreduce(loss_value)  # Average the loss across workers
     return loss_value
 
 
-for batch, (images, labels) in enumerate(dataset.take(1000 // dist.size())):
-    loss_value = training_step(images, labels, batch == 0)
+def train(mnist_dataset, mnist_model, mnist_loss, mnist_optimizer, mnist_epochs):
+    for batch, (images, labels) in enumerate(mnist_dataset.take(mnist_epochs // dist.size())):
+        loss_value = training_step(images, labels, batch == 0, mnist_model, mnist_loss, mnist_optimizer)
+        if batch % 50 == 0 and dist.rank() == 0:
+            print('Step #%d\tLoss: %.6f' % (batch, loss_value))
 
-    if batch % 50 == 0 and dist.rank() == 0:
-        print('Step #%d\tLoss: %.6f' % (batch, loss_value))
 
-# SMDataParallel: Save checkpoints only from master node.
-if dist.rank() == 0:
-    mnist_model.save(os.path.join(checkpoint_dir, '1'))
+if __name__ == "__main__":
+    dist.init()
+    config_gpus()
+    print("Worker number:", dist.rank())
+    epochs, batch_size, learning_rate = get_hyperparameters()
+    model, loss, optimizer, checkpoint = create_model(learning_rate)
+    dataset = get_dataset(batch_size)
+    train(dataset, model, loss, optimizer, epochs)
+    # SMDataParallel: Save checkpoints only from master node.
+    if dist.rank() == 0:
+        checkpoint.save("/opt/ml/model/1")
