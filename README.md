501ae2dfe0921ffb048a4291d5257fbce5227371
GrantPerkins <gcperk20@gmail.com>
2021-05-04 12:18:20 -0400

    Finish write up

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
 # Grant Perkins CS 4513 Final Project
 
-In this project, I developed a distributed machine learning solution with AWS SageMaker.
+Grant Perkins (gcperkins@wpi.edu)
+
+In this project, I developed a distributed machine learning solution with TensorFlow and AWS SageMaker.
 
 Features:
  - Distributed computing:
    + One virtual computer runs this Jupyter Notebook
-   + One virtual computer stores dataset
-   + N virtual computers run the training job
-   + One virtual computer runs the inference job (small load)
+   + One virtual computer stores dataset (in AWS S3)
+   + Multiple virtual computers run the training job
  - Machine Learning:
-   + Trains a custom neural network to recognize digits in the MNIST dataset
+   + Trains a convolutional neural network to recognize digits in the MNIST dataset
    + Customizable hyperparameters (epochs, batch size, learning rate)
-   + Inference on whatever image (classification)
 
 I started this project on 4/30/2021, for CS 4513.
 
-## Section 1: Data
+## Project details
+
+### Files
+`mnist.ipynb` contains a Jupyter notebook to run in SageMaker.
+`output.ipynb` shows the expected output if your run all the blocks from mnist.ipynb.
+`mnist.py` contains all of the actual machine learning code
+
+### Section 1: Data
 
 In this section, I download the dataset, and upload it to a computer controlled by AWS S3, a long-term data service.
+The dataset is split between all of the workers, so N slices of the dataset are uploaded to S3.
 
-## Section 2: Training
+### Section 2: Training
 
-Now that the data is in the correct place, I can train a neural network for the dataset. I do this on N separate computers. N can be any natural number.
+This step runs `mnist.py` across many workers. Data is downloaded from AWS S3. Each worker has a different "rank", and 
+this rank is used to determine which slice of the dataset the worker should use. The rank 0 worker is considered the
+master. This master worker prints metrics every 50 epochs. `mnist.py` trains a custom convolutional neural network. It
+calculates loss using cross entropy. I am using an Adam Optimizer.
+
+The majority of modifications between a normal MNIST model and mine is in the `training_step` function. This function 
+leverages a SageMaker API to calculate loss from all of the workers in the master worker.
 
--- code/mnist.py
+++ code/mnist.py
@@ -7,37 +7,57 @@ import smdistributed.dataparallel.tensorflow as dist
 
 
 def config_gpus():
+    """
+    Multiple GPUs are being used across the workers. This function initializes GPUs for use.
+    :return: None
+    """
     gpus = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
-
     tf.config.experimental.set_visible_devices(gpus[dist.local_rank()], 'GPU')
 
 
 def get_hyperparameters():
+    """
+    Read the hyperparameters specified in the Jupyter Notebook
+    :return: epochs, batch size, and learning rate
+    """
     with open("/opt/ml/input/config/hyperparameters.json", 'r') as f:
-        file = json.load(f)
-        mnist_epochs = int(file["epochs"])
-        mnist_batch_size = int(file["batch_size"])
-        mnist_learning_rate = float(file["learning_rate"])
+        # SageMaker writes the hyperparameters as a json file to the same place every time
+        hyper_file = json.load(f)
+        mnist_epochs = int(hyper_file["epochs"])
+        mnist_batch_size = int(hyper_file["batch_size"])
+        mnist_learning_rate = float(hyper_file["learning_rate"])
     return mnist_epochs, mnist_batch_size, mnist_learning_rate
 
 
 def get_dataset(mnist_batch_size):
-    data_slice = 'mnist-%d.npz' % dist.rank()
-    keras_path = os.path.join(str(Path.home()), ".keras/datasets/")
+    """
+    get the dataset slice that corresponds to the worker
+    :param mnist_batch_size: size of the batch. a hyperparameter
+    :return: a tf.data.Dataset object
+    """
+    data_slice_name = 'mnist-%d.npz' % dist.rank()
+    keras_path = os.path.join(str(Path.home()), ".keras/datasets/")  # keras expects data to be here
     os.makedirs(keras_path, exist_ok=True)
-    copyfile("/opt/ml/input/data/training/" + data_slice, keras_path + data_slice)
-    (mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data(path=data_slice)
-    mnist_dataset = tf.data.Dataset.from_tensor_slices(
+    copyfile("/opt/ml/input/data/training/" + data_slice_name,
+             keras_path + data_slice_name)  # copy file from magic S3 location to keras path
+    (mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data(path=data_slice_name)
+    # need to convert from 0-255 int pixel format to float, labels to ints
+    data_slice = tf.data.Dataset.from_tensor_slices(
         (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32),
          tf.cast(mnist_labels, tf.int64))
-    )
-    mnist_dataset = mnist_dataset.repeat().shuffle(100000).batch(mnist_batch_size)
-    return mnist_dataset
+    ).repeat().shuffle(100000).batch(mnist_batch_size)  # repeat infinitely, shuffle, and set batch size
+    return data_slice
 
 
 def create_model(mnist_learning_rate):
+    """
+    Creates a new keras model for learning
+    :param mnist_learning_rate: learning rate for the Adam Optimizer
+    :return: mode, loss function, and optimizer
+    """
+    # neural net
     mnist_model = tf.keras.Sequential([
         tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
         tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
@@ -49,45 +69,56 @@ def create_model(mnist_learning_rate):
         tf.keras.layers.Dense(10, activation='softmax')
     ])
     mnist_loss = tf.losses.SparseCategoricalCrossentropy()
-
-    # SMDataParallel: dist.size()
-    # LR for 8 node run : 0.000125
-    # LR for single node run : 0.001
+    # learning rate is proportional to number of workers
     mnist_optimizer = tf.optimizers.Adam(mnist_learning_rate * dist.size())
-    tf.train.Checkpoint(model=mnist_model, optimizer=optimizer)
     return mnist_model, mnist_loss, mnist_optimizer
 
 
 @tf.function
-def training_step(images, labels, first_batch):
+def training_step(images, labels, batch):
+    """
+    Runs one training step, with considerations for the distributed nature of the job.
+    :param images: images, sorted
+    :param labels: labels for images, same order
+    :param batch: batch number. variables must be broadcasted on first batch
+    :return: loss for this step across all workers
+    """
     with tf.GradientTape() as tape:
+        # record losses for automatic differentiation (learning)
         probs = model(images, training=True)
         loss_value = loss(labels, probs)
 
-    # SMDataParallel: Wrap tf.GradientTape with SMDataParallel's DistributedGradientTape
+    # need to wrap tape with more tape, because it is distributed
     tape = dist.DistributedGradientTape(tape)
 
     grads = tape.gradient(loss_value, model.trainable_variables)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))  # learn on this worker
 
-    if first_batch:
-        # SMDataParallel: Broadcast model and optimizer variables
+    if batch == 0:
+        # workers share learning broadcast by broadcasting variables. one set of variables across workers.
         dist.broadcast_variables(model.variables, root_rank=0)
         dist.broadcast_variables(optimizer.variables(), root_rank=0)
 
-    # SMDataParallel: all_reduce call
+    # all loss values from all workers reduced to one loss value
     loss_value = dist.oob_allreduce(loss_value)  # Average the loss across workers
     return loss_value
 
 
 def train(mnist_epochs):
+    """
+    Train CNN
+    :param mnist_epochs: number of training steps to run for
+    :return: None
+    """
     for batch, (images, labels) in enumerate(dataset.take(mnist_epochs // dist.size())):
-        loss_value = training_step(images, labels, batch == 0)
+        loss_value = training_step(images, labels, batch)
+        # print loss every 50 epochs in master worker
         if batch % 50 == 0 and dist.rank() == 0:
             print('Step #%d\tLoss: %.6f' % (batch, loss_value))
 
 
 if __name__ == "__main__":
+    # runs training, but distributed
     dist.init()
     config_gpus()
     print("Worker number:", dist.rank())
@@ -95,7 +126,8 @@ if __name__ == "__main__":
     model, loss, optimizer = create_model(learning_rate)
     dataset = get_dataset(batch_size)
     train(epochs)
-    # SMDataParallel: Save checkpoints only from master node.
+
+    # save model as master
     if dist.rank() == 0:
         checkpoint_dir = "/opt/ml/model"
         model.save(os.path.join(checkpoint_dir, '1'))
